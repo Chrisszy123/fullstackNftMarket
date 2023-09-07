5876bd860d26b4b630dd0410d8d32091a0e91e96
Chrisszy123 <bassygoodluck@gmail.com>
2023-09-07 12:40:54 +0100

    feat/ started working on discover component

--- client/components/Navbar/Discover/Discover.tsx
+++ client/components/Navbar/Discover/Discover.tsx
@@ -1,6 +1,38 @@
 import React from 'react'
-
+import Link from 'next/link'
+//internal imports
+import style from './Discover.module.css'
 const Discover = () => {
+  const discover = [
+    {
+      name: "Collection",
+      link: "collection"
+    },
+    {
+      name: "Search",
+      link: "seacrh"
+    },
+    {
+      name: "Author Profile",
+      link: "author-profile"
+    },
+    {
+      name: "NFT Details",
+      link: "NFT-details"
+    },
+    {
+      name: "Account Setting",
+      link: "account-setting"
+    },
+    {
+      name: "Connect Wallet",
+      link: "connect-wallet"
+    },
+    {
+      name: "Blog",
+      link: "blog"
+    },
+  ]
   return (
     <div>Discover</div>
   )
